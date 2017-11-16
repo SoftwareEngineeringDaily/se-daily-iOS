32a2f6cf3151a6bec36bddb03e01264d3b6fd2d3
Craig Holliday <themisterholliday@gmail.com>
2017-11-16 13:37:25 -0600

    Added download manager

---
+++ SEDaily-IOS/DownloadManager.swift
@@ -0,0 +1,13 @@
+//
+//  DownloadManager.swift
+//  SEDaily-IOS
+//
+//  Created by Craig Holliday on 11/16/17.
+//  Copyright © 2017 Koala Tea. All rights reserved.
+//
+
+import Foundation
+
+public class DownloadManager: NSObject {
+    
+}

b5d12db541a43c60f6b81a9a57d0022e5e916b59
Berk M <berkmolla@gmail.com>
2017-11-25 20:32:04 +0000

    Remove unused imports and performance example test from base unit test class.

--- SEDaily-IOSTests/SEDaily_IOSTests.swift
+++ SEDaily-IOSTests/SEDaily_IOSTests.swift
@@ -7,8 +7,6 @@
 //
 
 import XCTest
-import SwiftSoup
-import Atributika
 @testable import SEDaily_IOS
 
 class SEDaily_IOSTests: XCTestCase {
@@ -26,13 +24,6 @@ class SEDaily_IOSTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
     
 }

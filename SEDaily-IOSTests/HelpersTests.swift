7d2828dcb79a0acbc39558f8857c91f29462ea60
Berk M <berkmolla@gmail.com>
2017-11-25 20:31:29 +0000

    Add Helpers test file.

---
+++ SEDaily-IOSTests/HelpersTests.swift
@@ -0,0 +1,28 @@
+//
+//  HelpersTests.swift
+//  SEDaily-IOSTests
+//
+//  Created by Berk Mollamustafaoglu on 25/11/2017.
+//
+//
+
+import XCTest
+@testable import SEDaily_IOS
+
+class HelpersTests: SEDaily_IOSTests {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testIsValidEmailAddress_validEmailAddress() {
+     Help
+    }
+    
+}

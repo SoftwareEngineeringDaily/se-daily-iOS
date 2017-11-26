0614a32a44d05ab9e7d75f6bc0e9cfc59429770e
Berk M <berkmolla@gmail.com>
2017-11-26 20:43:02 +0000

    Create UserModelTests and UserDefaultsMock class.

---
+++ SEDaily-IOSTests/Mocks/UserDefaultsMock.swift
@@ -0,0 +1,9 @@
+//
+//  UserDefaultsMock.swift
+//  SEDaily-IOSTests
+//
+//  Created by Berk Mollamustafaoglu on 26/11/2017.
+//  Copyright © 2017 Koala Tea. All rights reserved.
+//
+
+import Foundation

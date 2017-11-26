488bafc0f939829cd5fa5a26738eb353f933ac0d
Berk M <berkmolla@gmail.com>
2017-11-26 21:26:05 +0000

    Delete unused setup/teardown methods.

--- SEDaily-IOSTests/HelpersTests.swift
+++ SEDaily-IOSTests/HelpersTests.swift
@@ -13,16 +13,6 @@ import Nimble
 
 class HelpersTests: QuickSpec {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
     override func spec() {
         describe("isValidEmailAddress") {
             it("accepts regular email address format") {

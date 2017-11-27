4ae28732288355034a1bfd95f698355dfc872d12
Berk M <berkmolla@gmail.com>
2017-11-27 21:37:00 +0000

    A couple more UserModel tests.

--- SEDaily-IOSTests/UserModelTests.swift
+++ SEDaily-IOSTests/UserModelTests.swift
@@ -16,39 +16,32 @@ class UserModelTests: QuickSpec {
     override func spec() {
         describe("userModel tests") {
             var userManager: UserManager!
+            var user: User!
             var encoder = JSONEncoder()
             var decoder = JSONDecoder()
             
             beforeEach {
-                let dict = [String : Any]()
+                let dict = [String:Any]()
                 userManager = UserManager(userDefaults: UserDefaultsMock(dict: dict))
+                
+                user = User(firstName: "firstName",
+                                lastName: "lastName",
+                                email: "email@email.com",
+                                token: "abcdefg")
+                userManager.setCurrentUser(to: user)
             }
-            
+
             describe("isCurrentUserLoggedIn tests") {
                 it("returns false when user is not logged in") {
+                    userManager.setCurrentUser(to: User())
                     expect(userManager.isCurrentUserLoggedIn()).to(beFalse())
                 }
-                
                 it("returns true when user is logged in") {
-                    let user = User(firstName: "firstName",
-                                    lastName: "lastName",
-                                    email: "email@email.com",
-                                    token: "abcdefg")
-                    userManager.setCurrentUser(to: user)
                     expect(userManager.isCurrentUserLoggedIn()).to(beTrue())
                 }
             }
             
             describe("getActiveUser tests") {
-                var user : User!
-                beforeEach {
-                    user = User(firstName: "firstName",
-                                    lastName: "lastName",
-                                    email: "email@email.com",
-                                    token: "abcdefg")
-                    userManager.setCurrentUser(to: user)
-                }
-                
                 it("returns the active user when the saved user matches the current user") {
                     expect(userManager.getActiveUser()).to(equal(user))
                 }
@@ -57,10 +50,44 @@ class UserModelTests: QuickSpec {
                     userManager.defaults.set(User(), forKey: "user")
                     expect(userManager.getActiveUser()).to(equal(user))
                 }
+            }
+            
+            describe("logout user") {
 
+                it("sets current user to empty") {
+                    userManager.logoutUser()
+                    expect(userManager.currentUser).to(equal(User()))
+                }
+            }
+            
+            describe("setCurrentUser tests") {
 
+                it("changes user if the value passed in is a different user") {
+                    let newUser = User(firstName: "firstName",
+                                       lastName: "lastName",
+                                       email: "email@email.com",
+                                       token: "abcdefg")
+                    
+                    userManager.setCurrentUser(to: newUser)
+                    expect(userManager.currentUser).to(equal(newUser))
+                }
+                
+                it("doesn't change user if the same user is passed in") {
+                    let localUser = User(firstName: "firstName",
+                                         lastName: "lastName",
+                                         email: "email@email.com",
+                                         token: "abcdefg")
+                    
+                    userManager.setCurrentUser(to: localUser)
+                    let sameUser = User(firstName: "firstName",
+                                        lastName: "lastName",
+                                        email: "email@email.com",
+                                        token: "abcdefg")
+                    
+                    userManager.setCurrentUser(to: sameUser)
+                    expect(userManager.currentUser).to(equal(localUser))
+                }
             }
         }
-        
     }
 }

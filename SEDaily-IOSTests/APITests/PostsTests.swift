470b1a401ce0c987dd4272f1de7d07f520af894d
Berk M <berkmolla@gmail.com>
2018-01-20 22:39:06 +0000

    Add a couple of tests for getPosts endpoint.

--- SEDaily-IOSTests/APITests/PostsTests.swift
+++ SEDaily-IOSTests/APITests/PostsTests.swift
@@ -16,7 +16,7 @@ class PostsTests: QuickSpec {
     
     override func spec() {
         describe("posts calls") {
-            it("returns a list of posts with a successful call") {
+            it("getPostsWith returns a list of posts with a successful call") {
                 // Setup
                 let path = Bundle(for: type(of: self)).path(forResource: "getPostsWith_success", ofType: "json")!
                 let data = NSData(contentsOfFile: path)!
@@ -36,7 +36,7 @@ class PostsTests: QuickSpec {
                 expect(successCalled).toEventually(beTrue())
             }
             
-            it("failure callback called on API call failure") {
+            it("getPostsWith failure callback called on API call failure") {
                 // Setup
                 let error = NSError(domain: "", code: 401, userInfo: nil)
                 self.stub(everything, failure(error))
@@ -45,6 +45,44 @@ class PostsTests: QuickSpec {
                 var successCalled: Bool? = nil, failureCalled: Bool? = nil
                 
                 api.getPostsWith(searchTerm: "", createdAtBefore: "", onSuccess: { podcasts in
+                    successCalled = true
+                }, onFailure: { _ in
+                    failureCalled = true
+                })
+                expect(failureCalled).toEventually(beTrue())
+                expect(successCalled).toEventually(beNil())
+                
+            }
+            
+            it("getPosts returns the top podcasts successfully") {
+                // Setup
+                let path = Bundle(for: type(of: self)).path(forResource: "getPosts_topPosts", ofType: "json")!
+                let data = NSData(contentsOfFile: path)!
+                self.stub(everything, jsonData(data as Data))
+                
+                let api = API()
+                var successCalled: Bool? = nil, failureCalled: Bool? = nil
+                
+                api.getPosts(type: "top", createdAtBefore: "", tags: "", categories: "", onSuccess: { (podcasts) in
+                    expect(podcasts).toNot(beNil())
+                    expect(podcasts).toNot(beEmpty())
+                    successCalled = true
+                }, onFailure: { _ in
+                    failureCalled = true
+                })
+                expect(failureCalled).toEventually(beNil())
+                expect(successCalled).toEventually(beTrue())
+            }
+            
+            it("getPosts failure callback called on API call failure") {
+                // Setup
+                let error = NSError(domain: "", code: 401, userInfo: nil)
+                self.stub(everything, failure(error))
+                
+                let api = API()
+                var successCalled: Bool? = nil, failureCalled: Bool? = nil
+                
+                api.getPosts(type: "top", createdAtBefore: "", tags: "", categories: "", onSuccess: { (podcasts) in
                     successCalled = true
                 }, onFailure: { _ in
                     failureCalled = true

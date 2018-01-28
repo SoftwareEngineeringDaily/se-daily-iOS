3b9926d6b9737b330e2c993fa798110d4e70fb42
jason <bjason@gmail.com>
2018-01-28 09:46:20 -0800

    Related Links table view

---
+++ SEDaily-IOS/RelatedLinksViewController.swift
@@ -0,0 +1,57 @@
+//
+//  RelatedLinksViewController.swift
+//  SEDaily-IOS
+//
+//  Created by jason on 1/28/18.
+//  Copyright © 2018 Koala Tea. All rights reserved.
+//
+
+import UIKit
+
+class RelatedLinksViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Related links")
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.estimatedRowHeight = 44
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension RelatedLinksViewController : UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+}

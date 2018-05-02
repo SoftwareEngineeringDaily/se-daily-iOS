57239151eb97f57193c6127f17327911c4896cc8
jason <bjason@gmail.com>
2018-05-02 11:13:59 -0700

    Remove localhosthost API used for testing

--- SEDaily-IOS/API.swift
+++ SEDaily-IOS/API.swift
@@ -73,10 +73,8 @@ extension API {
 }
 
 class API {
-//    private let prodRootURL = "https://software-enginnering-daily-api.herokuapp.com/api"
-    private let prodRootURL = "http://localhost:4040/api"
-    private let stagingRootURL = "http://localhost:4040/api"
-//    private let stagingRootURL = "https://sedaily-backend-staging.herokuapp.com/api"
+    private let prodRootURL = "https://software-enginnering-daily-api.herokuapp.com/api"
+    private let stagingRootURL = "https://sedaily-backend-staging.herokuapp.com/api"
 
     var rootURL: String {
         #if DEBUG

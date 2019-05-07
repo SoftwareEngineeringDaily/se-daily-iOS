b936da78fb4609e4ebbc28326dbee8f110fe4730
David <dawid.cedrych@gmail.com>
2019-05-07 13:16:39 +0200

    profileCell boilerplate

---
+++ SEDaily-IOS/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  SEDaily-IOS
+//
+//  Created by Dawid Cedrych on 5/7/19.
+//  Copyright © 2019 Koala Tea. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+	}
+		
+}
---
+++ SEDaily-IOS/SettingsCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsCell.swift
+//  SEDaily-IOS
+//
+//  Created by Dawid Cedrych on 5/7/19.
+//  Copyright © 2019 Koala Tea. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
